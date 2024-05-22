8bcd60cf576cef2806c37b661ba728e20aff1678
RazvanLucacii <rlucaci48@gmail.com>
2024-05-22 17:13:11 +0200

    Add project files.

---
+++ ConsoleAWSSecrets/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleAWSSecrets.Helpers;
+using ConsoleAWSSecrets.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Ejemplo secrets manager");
+string miSecreto = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine(miSecreto);
+
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
+Console.WriteLine("MySql: " + model.MySql);
